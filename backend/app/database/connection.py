e75c191c1cc7fd1a998637ea42b0a2cce09923a4
Miguel Dhein <migueldhein50@gmail.com>
2026-05-12 13:45:25 -0300

    Fix get_db import

--- backend/app/database/connection.py
+++ backend/app/database/connection.py
@@ -1,8 +1,13 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
-from app.core.config import settings
+from dotenv import load_dotenv
+import os
 
-engine = create_engine(settings.DATABASE_URL)
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+engine = create_engine(DATABASE_URL)
 
 SessionLocal = sessionmaker(
     autocommit=False,
@@ -11,3 +16,13 @@ SessionLocal = sessionmaker(
 )
 
 Base = declarative_base()
+
+
+def get_db():
+    db = SessionLocal()
+
+    try:
+        yield db
+
+    finally:
+        db.close()
