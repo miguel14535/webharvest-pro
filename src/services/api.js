ae521ab0c3880d04dfe1200510f2f6c3b203e5c6
Miguel Dhein <migueldhein50@gmail.com>
2026-05-12 16:41:22 -0300

    Deploy monitor routes

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://webharvest-pro.onrender.com/api",
+  baseURL: "https://webharvest-pro.onrender.com",
 });
 
 export default api;
